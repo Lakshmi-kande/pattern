4dfc6f421e0f1ab846792666791770931466eba4
Lakshmi-kande <110766660+Lakshmi-kande@users.noreply.github.com>
2022-08-07 16:40:30 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -18,3 +18,31 @@ for (let i=0;i<=r;i++){
 
 //right triangle triangle
 
+let n=5
+let string=''
+for (let i=1;i<=n;i++){
+  for (let j=1;j <= n-i;j++){
+    string+=''
+  }
+  for (let k=0;k<2*i-1;k++){
+    string+='*'
+  }
+  string+='\n'
+}console.log(string)
+// use  single string it get output right angle triangle
+
+let n = 5;
+let string = "";
+
+for (let i = 1; i <= n; i++) {
+  
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
+}console.log(string);
+// use double string it get output triangle
