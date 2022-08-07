c80aadd9da1808511631c4503ad9bbb90b9d6256
Lakshmi-kande <110766660+Lakshmi-kande@users.noreply.github.com>
2022-08-07 15:59:41 +0530

    Add files via upload

---
+++ pattern.js
@@ -0,0 +1,20 @@
+let p=5
+let n=''
+for (let i=0;i<=p;i++){
+  for (let k=0;k<=p-i;k++){
+    n+='*';
+  }n+='\n'
+}console.log(n)
+
+//reverse right triangle
+
+let r=5
+let n=''
+for (let i=0;i<=r;i++){
+  for (let k=0;k<=i;k++){
+    n+='*';
+  }n+='\n'
+}console.log(n)
+
+//right triangle triangle
+
